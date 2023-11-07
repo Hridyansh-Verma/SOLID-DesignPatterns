cf7a2dee65f367a8c43b6e90047b81932c418260
Hridyansh <hridyanshverma1234@gmail.com>
2023-11-07 18:52:25 +0530

    Learning OCP

---
+++ SOLID/src/OCP/Products.java
@@ -0,0 +1,14 @@
+package OCP;
+
+public class Products {
+    public Color color;
+    public Size size;
+    String name;
+
+    public Products(Color color, Size size, String name) {
+        this.color = color;
+        this.size = size;
+        this.name = name;
+    }
+}
+
---
+++ SOLID/src/OCP/Size.java
@@ -0,0 +1,5 @@
+package OCP;
+
+public enum Size {
+    TINY,SMALL,MEDIUM,LARGE,EXTRA_LARGE;
+}
